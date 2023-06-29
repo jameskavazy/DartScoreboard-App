d851fff23a3e3396f5cd3dee08675ceaf0a4e795
JamesK <james.kavazy@gmail.com>
2023-06-29 20:40:54 +0100

    - Add basic ui to GameActivity.

--- app/src/main/java/com/example/dartscoreboard/GameActivity.java
+++ app/src/main/java/com/example/dartscoreboard/GameActivity.java
@@ -4,19 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+
+    private TextView gameTitle;
+    private TextView playerName;
+    private TextView playerCurrentScore;
+
+    private Player testPlayer = new Player("Test1", 0);
+
+    private HomeActivity.GameType gameType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("dom test", "GameActivityonCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
-        Bundle arguments = getIntent().getExtras();
-        HomeActivity.GameType gameType = (HomeActivity.GameType) arguments.getSerializable(HomeActivity.GAME_TYPE_KEY);
-        Log.d("dom test", "gameType\n-------\nname " + gameType.name + "\nstartingScore " + gameType.startingScore);
+        Log.d("dom test", "gameType\n-------\nname " + getGameType().name + "\nstartingScore " + getGameType().startingScore);
+
+        setupUI();
     }
 
+    private void setupUI() {
+        gameTitle = findViewById(R.id.gameActivityTitle);
+        playerName = findViewById(R.id.gameActivityPlayerName);
+        playerCurrentScore = findViewById(R.id.gameActivityPoints);
+        gameTitle.setText(getGameType().name);
+        playerName.setText(testPlayer.name);
+        testPlayer.currentScore = getGameType().startingScore; // todo this may be called again
+        int i = testPlayer.currentScore;
+        playerCurrentScore.setText(String.valueOf(testPlayer.currentScore));
+    }
+
+    private HomeActivity.GameType getGameType() {
+        if (gameType != null) {
+            return gameType;
+        }
+
+        Bundle arguments = getIntent().getExtras();
+        gameType = (HomeActivity.GameType) arguments.getSerializable(HomeActivity.GAME_TYPE_KEY);
+        return gameType;
+    }
 
     class Player {
         String name;
