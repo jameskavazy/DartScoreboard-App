cb821c82e2a10b672405d28acb34b474ffe0bc56
JamesK <james.kavazy@gmail.com>
2023-06-29 21:24:09 +0100

    - Add basic score input.

--- app/src/main/java/com/example/dartscoreboard/GameActivity.java
+++ app/src/main/java/com/example/dartscoreboard/GameActivity.java
@@ -2,9 +2,16 @@ package com.example.dartscoreboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -12,8 +19,11 @@ public class GameActivity extends AppCompatActivity {
     private TextView gameTitle;
     private TextView playerName;
     private TextView playerCurrentScore;
-
+    private EditText inputScoreEditText;
     private Player testPlayer = new Player("Test1", 0);
+
+    private int currentTypedScore;
+
 
     private HomeActivity.GameType gameType;
 
@@ -31,11 +41,47 @@ public class GameActivity extends AppCompatActivity {
         gameTitle = findViewById(R.id.gameActivityTitle);
         playerName = findViewById(R.id.gameActivityPlayerName);
         playerCurrentScore = findViewById(R.id.gameActivityPoints);
+        inputScoreEditText = findViewById(R.id.inputScoreEditText);
+
         gameTitle.setText(getGameType().name);
         playerName.setText(testPlayer.name);
         testPlayer.currentScore = getGameType().startingScore; // todo this may be called again
         int i = testPlayer.currentScore;
         playerCurrentScore.setText(String.valueOf(testPlayer.currentScore));
+
+
+        inputScoreEditText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                Log.d("dom test", "IME_ACTION_DONE");
+                onScoreEntered(inputScoreEditText.getText().toString());
+                return true;
+            }
+            return false;
+        });
+
+//        inputScoreEditText.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+////                Log.d("dom test", "onTextChanged" + s);
+////                try {
+////                   currentTypedScore = Integer.parseInt((String) s);
+////                    Log.d("dom test", String.valueOf(currentTypedScore));
+////                } catch (NumberFormatException e) {
+////                    Log.d("dom test", e.getMessage());
+////                }
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//                Log.d("dom test", "afterTextChanged");
+//
+//            }
+//        });
     }
 
     private HomeActivity.GameType getGameType() {
@@ -47,6 +93,22 @@ public class GameActivity extends AppCompatActivity {
         gameType = (HomeActivity.GameType) arguments.getSerializable(HomeActivity.GAME_TYPE_KEY);
         return gameType;
     }
+
+    private void onScoreEntered(String scoreString) {
+        try {
+            // todo add logic to check valid input
+            int scoreInt = Integer.parseInt(scoreString);
+            Log.d("dom test", Integer.toString(scoreInt));
+
+            if (scoreInt > 180) {
+                Toast.makeText(GameActivity.this, "Score cannt be over 180", Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (NumberFormatException e) {
+            Log.d("dom test", e.getMessage());
+        }
+    }
+
 
     class Player {
         String name;
