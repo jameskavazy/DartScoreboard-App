825ab9b4368ab8da933fc9ccefb2de4a18634686
JamesK <james.kavazy@gmail.com>
2023-08-01 19:21:55 +0100

    - Adds a multiselect box for selecting players

--- app/src/main/java/com/example/dartscoreboard/SelectGameActivity.java
+++ app/src/main/java/com/example/dartscoreboard/SelectGameActivity.java
@@ -1,15 +1,20 @@
 package com.example.dartscoreboard;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class SelectGameActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,11 +23,12 @@ public class SelectGameActivity extends AppCompatActivity implements View.OnClic
     private Button startGameBtn;
 
     String[] gameSelectList = {"501","301","170"};
-
+    boolean[] selectedPlayers;
+    ArrayList<Integer> playerList = new ArrayList<>();
+    String[] listOfPlayers = {"Dom","James"};
     AutoCompleteTextView autoCompleteTextView;
-
+    AutoCompleteTextView playerListCheckBox;
     ArrayAdapter<String> adapterItems;
-
 
 
     @Override
@@ -36,16 +42,21 @@ public class SelectGameActivity extends AppCompatActivity implements View.OnClic
         Log.d("dom test", "setupUI");
         setContentView(R.layout.activity_main);
         startGameBtn = findViewById(R.id.gameStartButton);
+        playerListCheckBox = findViewById(R.id.NameDropDownBox);
         startGameBtn.setOnClickListener(this);
-        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
-        setUpDropDownMenu();
+        autoCompleteTextView = findViewById(R.id.gameTypeDropDownBox);
+        setUpGameTypeDropDownMenu();
+        selectedPlayers = new boolean[listOfPlayers.length];
+        playerListCheckBox.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.gameStartButton){
             openGameActivity();
         }
-
+        if (v.getId() == R.id.NameDropDownBox){
+            alertDialogueLaunch();
+        }
     }
 
     public void openGameActivity(){
@@ -61,10 +72,10 @@ public class SelectGameActivity extends AppCompatActivity implements View.OnClic
             Log.d("dom test","openSevenoGameActivity");
             openSevenoGameActivity();
         }
+        else Toast.makeText(this,"You must select a game to play!",Toast.LENGTH_SHORT).show();
     }
 
     private void openFiveoGameActivity() {
-
         Log.d("dom test", "openFiveoGameActivity");
         Bundle arguments = new Bundle();
         arguments.putSerializable("GAME_TYPE", GameType.FiveO);
@@ -91,11 +102,64 @@ public class SelectGameActivity extends AppCompatActivity implements View.OnClic
         startActivity(intent);
     }
 
-    private void setUpDropDownMenu(){
+    private void setUpGameTypeDropDownMenu(){
     adapterItems = new ArrayAdapter<String>(this,R.layout.list_item,gameSelectList);
     autoCompleteTextView.setAdapter(adapterItems);
     }
 
+
+    private void alertDialogueLaunch(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(SelectGameActivity.this);
+        builder.setTitle("Select Players");
+        builder.setCancelable(false);
+        builder.setMultiChoiceItems(listOfPlayers, selectedPlayers, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                if (isChecked){
+                    playerList.add(which);
+                    Collections.sort(playerList);
+
+                }
+                else playerList.remove(Integer.valueOf(which));
+            }
+
+
+        });
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int j = 0; j < playerList.size(); j++){
+                    stringBuilder.append(listOfPlayers[playerList.get(j)]);
+                    if (j != playerList.size() - 1) {
+                        stringBuilder.append(", ");
+                    }
+
+                }
+                playerListCheckBox.setText(stringBuilder.toString());
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNeutralButton("Clear All", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                for (int j = 0; j > selectedPlayers.length; j++){
+                    selectedPlayers[j] = false;
+                    playerList.clear();
+                    playerListCheckBox.setText(""); //todo change the XML so that it's a normal textview, then clear all will work as intended.
+
+                }
+            }
+        });
+        builder.show();
+    }
 
     enum GameType {
         FiveO("501", 501),
