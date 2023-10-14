cf6784eab4aa922ce30b605c470246ab39d3ade5
JamesK <james.kavazy@gmail.com>
2023-10-14 14:02:05 +0100

    - Randomises List of Users for Game - Updates playersListCheckBox to display new order

--- app/src/main/java/com/example/dartscoreboard/GameActivity.java
+++ app/src/main/java/com/example/dartscoreboard/GameActivity.java
@@ -18,10 +18,6 @@ public class GameActivity extends AppCompatActivity {
 
 
     private TextView gameTitle;
-    private TextView playerName;
-    private TextView playerNameTwo;
-    private TextView playerCurrentScore;
-    private TextView playerCurrentScoreTwo;
     private EditText inputScoreEditText;
     private Player testPlayer = new Player("test 1", 0, true);
     private Player testPlayer2 = new Player("test 2",0,false);
@@ -41,30 +37,24 @@ public class GameActivity extends AppCompatActivity {
 
     private void setupUI() {
         gameTitle = findViewById(R.id.gameActivityTitle);
-        playerName = findViewById(R.id.gameActivityPlayerOneName);
-        playerCurrentScore = findViewById(R.id.gameActivityPlayerCurrentScore);
-        playerNameTwo = findViewById(R.id.gameActivityPlayerTwoName);
-        playerCurrentScoreTwo = findViewById(R.id.gameActivityPlayerTwoCurrentScore);
         inputScoreEditText = findViewById(R.id.inputUserNameEditText);
-        addPlayerNames();
-
-
+        //addPlayerNames();
 
 
         gameTitle.setText(getGameType().name);
 
 
-        testPlayer.currentScore = getGameType().startingScore;// todo this may be called again
-        testPlayer2.currentScore = getGameType().startingScore;
-        playerCurrentScore.setText(valueOf(testPlayer.currentScore));
-        playerCurrentScoreTwo.setText(valueOf(testPlayer2.currentScore));
-
-            if (testPlayer.playerTurn) {
-                testPlayer.turn();
-            }
-            else {
-                testPlayer2.turn();
-            }
+//        testPlayer.currentScore = getGameType().startingScore;// todo this may be called again
+//        testPlayer2.currentScore = getGameType().startingScore;
+//        playerCurrentScore.setText(valueOf(testPlayer.currentScore));
+//        playerCurrentScoreTwo.setText(valueOf(testPlayer2.currentScore));
+//
+//            if (testPlayer.playerTurn) {
+//                testPlayer.turn();
+//            }
+//            else {
+//                testPlayer2.turn();
+//            }
     }
     private SelectGameActivity.GameType getGameType() {
         if (gameType != null) {
@@ -76,186 +66,138 @@ public class GameActivity extends AppCompatActivity {
         return gameType;
     }
 
-    private void addPlayerNames(){
-//        Bundle arguments = getIntent().getExtras();
-//        String nameToAdd = arguments.getString("PLAYER_NAME");
-//        String nameToAdd2 = arguments.getString("PLAYER_NAME_2");
-
-
-//        ArrayList<User> playersForGame = new ArrayList<>();
-//        for (int i = 0; i <playersForGame.size(); i++){
+//    private void addPlayerNames(){
+////        Bundle arguments = getIntent().getExtras();
+////        String nameToAdd = arguments.getString("PLAYER_NAME");
+////        String nameToAdd2 = arguments.getString("PLAYER_NAME_2");
 //
+//
+////        ArrayList<User> playersForGame = new ArrayList<>();
+////        for (int i = 0; i <playersForGame.size(); i++){
+////
+//////        }
+////        playerName.setText(testPlayer.name = PrefConfig.readUsersForGameSP(this).get(0).getUsername());
+////        Log.d("dom test",PrefConfig.readUsersForGameSP(this).get(0).getUsername());
+////        playerNameTwo.setText(testPlayer2.name = PrefConfig.readUsersForGameSP(this).get(1).getUsername());
+//
+//    }
+
+//    private void onScoreEntered(String scoreString) {
+//        try {
+//            if (testPlayer.playerTurn) {
+//                int scoreInt = Integer.parseInt(scoreString);
+//                Log.d("dom test", Integer.toString(scoreInt));
+//                testPlayer.currentScore = subtract(testPlayer.currentScore, scoreInt);
+//                Log.d("dom test","score subtract run");
+//                playerCurrentScore.setText(String.valueOf(testPlayer.currentScore));
+//                Log.d("dom test","Current Score: " + testPlayer.currentScore);
+//            }
+//
+//            else {
+//                int scoreInt = Integer.parseInt(scoreString);
+//                Log.d("dom test", Integer.toString(scoreInt));
+//                testPlayer2.currentScore = subtract(testPlayer2.currentScore, scoreInt);
+//                playerCurrentScoreTwo.setText(String.valueOf(testPlayer2.currentScore));
+//                Log.d("dom test","Current Score: " + testPlayer2.currentScore);
+//            }
+//
+//
+//        } catch (NumberFormatException e) {
+//            Log.d("dom test", e.getMessage());
 //        }
-        playerName.setText(testPlayer.name = PrefConfig.readUsersForGameSP(this).get(0).getUsername());
-        Log.d("dom test",PrefConfig.readUsersForGameSP(this).get(0).getUsername());
-        playerNameTwo.setText(testPlayer2.name = PrefConfig.readUsersForGameSP(this).get(1).getUsername());
-
-    }
-
-    private void onScoreEntered(String scoreString) {
-        try {
-            if (testPlayer.playerTurn) {
-                int scoreInt = Integer.parseInt(scoreString);
-                Log.d("dom test", Integer.toString(scoreInt));
-                testPlayer.currentScore = subtract(testPlayer.currentScore, scoreInt);
-                Log.d("dom test","score subtract run");
-                playerCurrentScore.setText(String.valueOf(testPlayer.currentScore));
-                Log.d("dom test","Current Score: " + testPlayer.currentScore);
-            }
-
-            else {
-                int scoreInt = Integer.parseInt(scoreString);
-                Log.d("dom test", Integer.toString(scoreInt));
-                testPlayer2.currentScore = subtract(testPlayer2.currentScore, scoreInt);
-                playerCurrentScoreTwo.setText(String.valueOf(testPlayer2.currentScore));
-                Log.d("dom test","Current Score: " + testPlayer2.currentScore);
-            }
-
-
-        } catch (NumberFormatException e) {
-            Log.d("dom test", e.getMessage());
-        }
-    }
+//    }
 
 
 
-    public int subtract(int playerScore, int currentTypedScore) {
-        int playerIndicator = Color.rgb(42,213,114);
-        int newScore = playerScore - currentTypedScore;
-        if (( ((playerScore <= 180) && (playerScore >= 171)) || (playerScore == 169) || (playerScore == 168) || (playerScore == 166) || (playerScore == 165) || (playerScore == 163) || (playerScore == 162) || (playerScore == 159)) && (currentTypedScore == playerScore)){
-            if (testPlayer.playerTurn) {
-                playerName.setBackgroundColor(playerIndicator);
-                playerNameTwo.setBackgroundColor(Color.WHITE);
-                testPlayer.playerTurn = true;
-                testPlayer2.playerTurn = false;
-            }
-            else {
-                playerNameTwo.setBackgroundColor(playerIndicator);
-                playerName.setBackgroundColor(Color.WHITE);
-                testPlayer.playerTurn = false;
-                testPlayer2.playerTurn = true;
-
-            }
-            Toast.makeText(GameActivity.this, "Invalid Score", Toast.LENGTH_SHORT).show();
-            return playerScore;
-        }
-
-        if (currentTypedScore > 180) {
-            if (testPlayer.playerTurn) {
-                playerName.setBackgroundColor(playerIndicator);
-                playerNameTwo.setBackgroundColor(Color.WHITE);
-                testPlayer.playerTurn = true;
-                testPlayer2.playerTurn = false;
-            }
-            else {
-                playerNameTwo.setBackgroundColor(playerIndicator);
-                playerName.setBackgroundColor(Color.WHITE);
-                testPlayer.playerTurn = false;
-                testPlayer2.playerTurn = true;
-            }
-            Toast.makeText(GameActivity.this, "Invalid Score", Toast.LENGTH_SHORT).show();
-            return playerScore;
-        }
-
-        if (newScore > 1) {
-            if (testPlayer.playerTurn) {
-                playerNameTwo.setBackgroundColor(playerIndicator);
-                playerName.setBackgroundColor(Color.WHITE);
-                testPlayer.playerTurn = false;
-                testPlayer2.playerTurn = true;
-            } else {
-                playerName.setBackgroundColor(playerIndicator);
-                playerNameTwo.setBackgroundColor(Color.WHITE);
-                testPlayer2.playerTurn = false;
-                testPlayer.playerTurn = true;
-            }
-            return newScore;
-        }
-        if (newScore == 0) {
-            if (testPlayer.playerTurn) {
-                playerName.setBackgroundColor(playerIndicator);
-                playerNameTwo.setBackgroundColor(Color.WHITE);
-                Toast.makeText(GameActivity.this, testPlayer.name + " wins!", Toast.LENGTH_LONG).show();
-            } else {
-                playerNameTwo.setBackgroundColor(playerIndicator);
-                playerName.setBackgroundColor(Color.WHITE);
-                Toast.makeText(GameActivity.this, testPlayer2.name + " wins!", Toast.LENGTH_LONG).show();
-            }
-            return newScore;
-        }
-        else {
-            if (testPlayer.playerTurn) {
-
-                testPlayer.playerTurn = false;
-                testPlayer2.playerTurn = true;
-                playerNameTwo.setBackgroundColor(playerIndicator);
-                playerName.setBackgroundColor(Color.WHITE);
-            }
-            else {
-                testPlayer.playerTurn = true;
-                testPlayer2.playerTurn = false;
-                playerName.setBackgroundColor(playerIndicator);
-                playerNameTwo.setBackgroundColor(Color.WHITE);
-
-            }
-            Toast.makeText(GameActivity.this, "BUST", Toast.LENGTH_SHORT).show();
-
-        return playerScore;
-        }
-
-    }
-
-
-
-    class Player {
-        String name;
-        int currentScore;
-        boolean playerTurn;
-
-        int lastScoreEntered;
+//    public int subtract(int playerScore, int currentTypedScore) {
+//        int playerIndicator = Color.rgb(42,213,114);
+//        int newScore = playerScore - currentTypedScore;
+//        if (( ((playerScore <= 180) && (playerScore >= 171)) || (playerScore == 169) || (playerScore == 168) || (playerScore == 166) || (playerScore == 165) || (playerScore == 163) || (playerScore == 162) || (playerScore == 159)) && (currentTypedScore == playerScore)){
+//            if (testPlayer.playerTurn) {
+//                playerName.setBackgroundColor(playerIndicator);
+//                playerNameTwo.setBackgroundColor(Color.WHITE);
+//                testPlayer.playerTurn = true;
+//                testPlayer2.playerTurn = false;
+//            }
+//            else {
+//                playerNameTwo.setBackgroundColor(playerIndicator);
+//                playerName.setBackgroundColor(Color.WHITE);
+//                testPlayer.playerTurn = false;
+//                testPlayer2.playerTurn = true;
+//
+//            }
+//            Toast.makeText(GameActivity.this, "Invalid Score", Toast.LENGTH_SHORT).show();
+//            return playerScore;
+//        }
+//
+//        if (currentTypedScore > 180) {
+//            if (testPlayer.playerTurn) {
+//                playerName.setBackgroundColor(playerIndicator);
+//                playerNameTwo.setBackgroundColor(Color.WHITE);
+//                testPlayer.playerTurn = true;
+//                testPlayer2.playerTurn = false;
+//            }
+//            else {
+//                playerNameTwo.setBackgroundColor(playerIndicator);
+//                playerName.setBackgroundColor(Color.WHITE);
+//                testPlayer.playerTurn = false;
+//                testPlayer2.playerTurn = true;
+//            }
+//            Toast.makeText(GameActivity.this, "Invalid Score", Toast.LENGTH_SHORT).show();
+//            return playerScore;
+//        }
+//
+//        if (newScore > 1) {
+//            if (testPlayer.playerTurn) {
+//                playerNameTwo.setBackgroundColor(playerIndicator);
+//                playerName.setBackgroundColor(Color.WHITE);
+//                testPlayer.playerTurn = false;
+//                testPlayer2.playerTurn = true;
+//            } else {
+//                playerName.setBackgroundColor(playerIndicator);
+//                playerNameTwo.setBackgroundColor(Color.WHITE);
+//                testPlayer2.playerTurn = false;
+//                testPlayer.playerTurn = true;
+//            }
+//            return newScore;
+//        }
+//        if (newScore == 0) {
+//            if (testPlayer.playerTurn) {
+//                playerName.setBackgroundColor(playerIndicator);
+//                playerNameTwo.setBackgroundColor(Color.WHITE);
+//                Toast.makeText(GameActivity.this, testPlayer.name + " wins!", Toast.LENGTH_LONG).show();
+//            } else {
+//                playerNameTwo.setBackgroundColor(playerIndicator);
+//                playerName.setBackgroundColor(Color.WHITE);
+//                Toast.makeText(GameActivity.this, testPlayer2.name + " wins!", Toast.LENGTH_LONG).show();
+//            }
+//            return newScore;
+//        }
+//        else {
+//            if (testPlayer.playerTurn) {
+//
+//                testPlayer.playerTurn = false;
+//                testPlayer2.playerTurn = true;
+//                playerNameTwo.setBackgroundColor(playerIndicator);
+//                playerName.setBackgroundColor(Color.WHITE);
+//            }
+//            else {
+//                testPlayer.playerTurn = true;
+//                testPlayer2.playerTurn = false;
+//                playerName.setBackgroundColor(playerIndicator);
+//                playerNameTwo.setBackgroundColor(Color.WHITE);
+//
+//            }
+//            Toast.makeText(GameActivity.this, "BUST", Toast.LENGTH_SHORT).show();
+//
+//        return playerScore;
+//        }
+//
+//    }
 
 
 
-        Player(String name, int currentScore, boolean playerTurn) {
-            this.name = name;
-            this.currentScore = currentScore;
-            this.playerTurn = playerTurn;
-        }
 
-        private void turn(){
-
-
-            //determines score for processing
-                inputScoreEditText.setOnEditorActionListener((v, actionId, event) -> {
-                    if (testPlayer.playerTurn) {
-                        if (actionId == EditorInfo.IME_ACTION_DONE) {
-                            Log.d("dom test", "IME_ACTION_DONE");
-                            onScoreEntered(inputScoreEditText.getText().toString());
-                            ((EditText) findViewById(R.id.inputUserNameEditText)).getText().clear();
-                            return true;
-                        }
-
-                    }
-                    else {
-                        if (actionId == EditorInfo.IME_ACTION_DONE) {
-                            Log.d("dom test", "IME_ACTION_DONE");
-                            onScoreEntered(inputScoreEditText.getText().toString());
-                            ((EditText) findViewById(R.id.inputUserNameEditText)).getText().clear();
-                            return true;
-                        }
-
-                    }
-                    return false;
-                });
-
-        }
-
-
-        private void printOutDetails() {
-            Log.d("dom test", "Name: " + name);
-        }
-
-    }
 
 
 
